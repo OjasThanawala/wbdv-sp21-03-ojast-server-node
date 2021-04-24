e35666720ee12476d005551527230e2af39aa2ae
Ojas Thanawala <ojas.thanawala@gmail.com>
2021-04-24 15:29:39 -0400

    a7 deployed

--- server.js
+++ server.js
@@ -15,4 +15,4 @@ app.use(function (req, res, next) {
 require('./controllers/questions-controller')(app)
 require('./controllers/quizzes-controller')(app)
 
-app.listen(4000)
+app.listen(process.env.PORT||4000)
